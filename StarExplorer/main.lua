e4011bcf97f5f289c91a3a542a04f1efb4bf6b6c
Bruno Barrios <bbarrios@number8.com>
2019-07-30 23:05:41 -0400

    Added Start Explorer images, sprite sheet

--- StarExplorer/main.lua
+++ StarExplorer/main.lua
@@ -5,3 +5,17 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+local physics = require("physics")
+physics.start()
+physics.setGracity(0, 0)
+
+--Seed the random number generator
+math.randomseed(os.time())
+
+--configure image sheet
+local sheetOptions = {
+    frames = {
+
+    },
+}
