f7efd40469849fba608a04d7305f0ba73fb834c5
Bruno Barrios <bbarrios@number8.com>
2019-09-05 07:26:54 -0400

    Added scenes

---
+++ StarExplorer/menu.lua
@@ -0,0 +1,86 @@
+local composer = require( "composer" )
+ 
+local scene = composer.newScene()
+
+
+local function gotoGame()
+    composer.gotoScene( "game", { time=800, effect="crossFade" } )
+end
+ 
+local function gotoHighScores()
+    composer.gotoScene( "highscores", { time=800, effect="crossFade" } )
+end
+
+--create
+function scene:create( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs when the scene is first created but has not yet appeared on screen
+    
+    local background = display.newImageRect( sceneGroup, "images/universe.png", 800, 1400 )
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
+
+    local title = display.newImageRect( sceneGroup, "images/title.png", 500, 80 )
+    title.x = display.contentCenterX
+    title.y = 200
+
+    local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, 700, native.systemFont, 44 )
+    playButton:setFillColor( 0.82, 0.86, 1 )
+ 
+    local highScoresButton = display.newText( sceneGroup, "High Scores", display.contentCenterX, 810, native.systemFont, 44 )
+    highScoresButton:setFillColor( 0.75, 0.78, 1 )
+
+    --added tap functionality to newText
+    playButton:addEventListener( "tap", gotoGame )
+    highScoresButton:addEventListener( "tap", gotoHighScores )
+end
+
+
+-- show()
+function scene:show( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is still off screen (but is about to come on screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs when the scene is entirely on screen
+ 
+    end
+end
+ 
+ 
+-- hide()
+function scene:hide( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is on screen (but is about to go off screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs immediately after the scene goes entirely off screen
+ 
+    end
+end
+ 
+ 
+-- destroy()
+function scene:destroy( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs prior to the removal of scene's view
+
+end
+
+---Listeners
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+return scene
